e6a3dd2a7ae83a418a0ddcb472060541cbcf9001
Edlly Andrea Gómez Cervantes <egomez@growthaccelerationpartners.com>
2016-04-01 18:57:49 -0600

    Added files via upload

---
+++ androidappium/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Androidappium::Application.config.secret_token = 'ec4664ffc8bbd9f2f4f000174e7b7cc6d93e7f58f079fdf65d815b93be7fd7b3de001b3a7c27cb9f15239fabe109cafe28373d12f4794e9d94c5e6749f852964'
