a4585976dba25dbf352feae0552f0e8fb35b7150
achevez <achevez@growthaccelerationpartners.com>
2016-06-25 11:50:20 -0600

    Add Test scenario 3

--- androidappium/spec/pages/home_page.rb
+++ androidappium/spec/pages/home_page.rb
@@ -2,13 +2,20 @@ require 'spec_helper'
 
 module Pages
   class HomePage
+    def product_search
+      first_textfield
+    end
 
-      def product_search
-        first_textfield
-      end
+    def supplier_search
+      textfield(2)
+    end
 
-      def supplier_search
-        textfield(2)
+    def sage_news
+      wait { text('SAGE News') }
+    end
+
+    def featured_product
+      wait { text('Featured Product') }
     end
   end
 end
